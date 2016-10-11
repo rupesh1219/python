dbb13ad0adb143093c14488c3e53c877aaca54ef
rupesh1219 <rupesh1219@gmail.com>
2016-10-11 18:01:10 -0400

    using list comprehenison and generators

--- project_euler/problem1.py
+++ project_euler/problem1.py
@@ -13,3 +13,35 @@ def sum_null():
     print('sum of all multiples of 3 or 5 below 1000 {}'.format(count))
 
 sum_null()
+
+'''
+using list comprehension
+'''
+
+list = [i for i in range(1000) if i%3==0 or i%5==0]
+print(sum(list))
+
+'''
+using generator
+
+In the above example its not worth creating a list in the memory
+as we are trying to find the sum.
+
+How do we avoid not creating a list and find the sum?
+'''
+
+print(sum(i for i in range(1000) if i%3 == 0 or i%5 ==0))
+
+def is_divisible_by_any(numerator,denominators):
+    for denominator in denominators:
+        if numerator % denominator == 0:
+            return True
+        return False
+
+def sum_divisible(max_range,denominators):
+    return sum(
+        i for i in range(max_range)
+        if is_divisible_by_any(i,denominators)
+    )
+
+print(sum_divisible(1000,[3,5]))
