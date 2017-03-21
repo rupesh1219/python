64b7f4b9cc64b7e041740570f31b2d61b97d207b
rupesh1219 <rupesh1219@gmail.com>
2017-03-20 22:42:08 -0400

    tuples refreshed

--- Data_Structures/practice/tuples.py
+++ Data_Structures/practice/tuples.py
@@ -128,5 +128,117 @@ print(list_tuple([1,2,3]))
 
 # write a program to remove an item from tuple
 
-def remove_element(tuple):
+def remove_element(tuple_items, value):
+    '''
+    tuples are immutable so we
+    cannot remove elements from
+    tuple
+    '''
+    list_items = list(tuple_items)
+    list_result = list_items.remove(value)
+    return tuple(list_result)
 
+print(remove_element((1,2,3), 3))
+
+########################################################################
+
+# write a program to slice a tuple
+
+def slice_tuple(tuple_items):
+    if len(tuple_items) % 2 == 0:
+        result1 = tuple_items[:int(len(tuple_items)/2)]
+        result2 = tuple_items[int(len(tuple_items)/2):]
+    else:
+        result1 = tuple_items[:int((len(tuple_items)/2 + 1))]
+        result2 = tuple_items[(int(len(tuple_items)/2 + 1)):]
+    return result1, result2
+
+print(slice_tuple((1,2,3,4,5,6)))
+
+########################################################################
+
+# write a python program to find the index of an item in a tuple
+
+def index_item(tuple, n):
+    return tuple.index(n)
+
+print(index_item((1,2,3,4),4))
+
+########################################################################
+
+# convert tuple of tuples to a dictionary
+
+def tuple_dict(tuple):
+    '''
+    converting tuple of tuples
+    to a dictonary
+    '''
+    dict_result = dict((k,v) for k,v in tuple)
+    return dict_result
+
+print(tuple_dict(((1,2),(2,3))))
+
+########################################################################
+
+# unzip a list of tuples in to individual lists
+
+def tuple_list(tuple):
+    '''
+    unzip list of tuples
+    into individual lists
+    '''
+    for i in tuple:
+        print(list(i))
+
+tuple_list(((1,2),(3,4),(6,7)))
+
+########################################################################
+
+# write a program to reverse a tuple
+
+def reverse_tuple(tuple):
+    return tuple[::-1]
+
+print(reverse_tuple((1,2,3)))
+
+########################################################################
+
+# write a program to convert list of tuples into a dictionary
+
+def list_tup_dict(list):
+    '''
+    list of tuples to
+    a dictionary
+    '''
+    dict_result = dict((k,v) for k,v in list)
+    return dict_result
+
+print(list_tup_dict([(1,2),(2,3),(3,4)]))
+
+########################################################################
+
+# print a tuple with string formatting
+
+def string_tuple(tuple):
+    return 'this is a tuple {}'.format(tuple)
+
+print(string_tuple((1,2,3)))
+
+########################################################################
+
+# write a program to replace a last element is list of tuples
+
+def list_tuples(list_items, replace_element):
+    '''
+    replace last element
+    in list of tuples
+    '''
+    for i in list_items:
+        a = list(i)
+        a[-1:] = replace_element
+        list_items[i] = tuple(a)
+    return list_items
+
+print(list_tuples([(1,2,3), (3,4,5), (5,6,7)], 9))
+
+########################################################################
