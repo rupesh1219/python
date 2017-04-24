aef96afe58afd5160830bea4e2cc5082d7618aee
rupesh1219 <rupesh1219@gmail.com>
2017-04-24 10:34:09 -0400

    hacker rank refreshed

---
+++ hackerrank/classes/torsional_angle.py
@@ -0,0 +1,11 @@
+###############################################################################
+# you are given four points A,B,C and D in a three dimensional plane
+# find the angle between the planes formed by A,B,C and B,C,D in degrees
+# let tehe angle by PHI
+# cos(PHI) = x.y/|x||y| where x = AB * BC and y = BC *CD
+###############################################################################
+
+class Angle():
+
+    def __init__(self, x, y, z):
+        self.x =
---
+++ hackerrank/introduction/print_function.py
@@ -0,0 +1,16 @@
+###############################################################################
+# read an integer N
+# without using any string methods print 123...N
+###############################################################################
+
+class Printfunction():
+
+    def __init__(self, x):
+        self.x = x
+
+    def printnum(self):
+        '''
+        print the sequence
+        '''
+        for i in range(self.x + 1):
+
